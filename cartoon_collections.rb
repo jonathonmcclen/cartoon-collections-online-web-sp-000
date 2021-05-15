dca5ad6b2a19104d3daf344cdda96ee170682fec
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-15 20:33:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,9 +18,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
   cheese_types.each do |cheese|
-    if array.find(cheese)
-      return cheese
-    end 
+    array.find(cheese)
   end
   nil
 end
